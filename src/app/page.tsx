bfb7c9b94fa77e59df29ff54e56b7b2b1901f7f9
Kivi Corn <kivi.webdev@gmail.com>
2023-07-18 22:49:31 +0100

    STYLE: priority buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,17 +20,38 @@ export default function Home() {
       </form>
 
       <div className="flex flex-col gap-4">
-        <div className="block max-w-md p-6 bg-pink-50 border border-gray-200 rounded-lg shadow">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <div className="flex gap-2">
+          <div className="flex flex-col gap-2 pt-2">
+            <div className="w-5 h-5 rounded-full bg-red-400"></div>
+            <div className="w-5 h-5 rounded-full bg-orange-400"></div>
+            <div className="w-5 h-5 rounded-full bg-green-400"></div>
+          </div>
+          <div className="block max-w-md p-6 bg-pink-50 border border-gray-200 rounded-lg shadow">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          </div>
         </div>
-        <div className="block max-w-md p-6 bg-orange-50 border border-gray-200 rounded-lg shadow">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <div className="flex gap-2">
+          <div className="flex flex-col gap-2 pt-2">
+            <div className="w-5 h-5 rounded-full bg-red-400"></div>
+            <div className="w-5 h-5 rounded-full bg-orange-400"></div>
+            <div className="w-5 h-5 rounded-full bg-green-400"></div>
+          </div>
+          <div className="block max-w-md p-6 bg-orange-50 border border-gray-200 rounded-lg shadow">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          </div>
         </div>
-        <div className="block max-w-md p-6 bg-green-50 border border-gray-200 rounded-lg shadow">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <div className="flex gap-2">
+          <div className="flex flex-col gap-2 pt-2">
+            <div className="w-5 h-5 rounded-full bg-red-400"></div>
+            <div className="w-5 h-5 rounded-full bg-orange-400"></div>
+            <div className="w-5 h-5 rounded-full bg-green-400"></div>
+          </div>
+          <div className="block max-w-md p-6 bg-green-50 border border-gray-200 rounded-lg shadow">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          </div>
         </div>
       </div>
     </main>
